0153aa6e7c7303c03e8fd62cfc7724fb5bd5ad50
Jacquimik <j.mikulasev@hotmail.com>
2014-11-09 22:44:37 +1100

    heading

--- burning_airlines/app/assets/javascripts/burningairlines.js
+++ burning_airlines/app/assets/javascripts/burningairlines.js
@@ -3,6 +3,27 @@
   	if ($('#main').length === 0) {
   	return;
   };
+
+//Jacqui start
+
+var obj = $('#fire');
+var fps = 200;
+var letters = obj.html().split('');
+obj.empty();
+$.each(letters,function(el){
+    obj.append($('<span>'+this+'</span>'));
+});
+var animateLetters = obj.find('span');
+setInterval(function(){
+    animateLetters.each(function(){
+        $(this).css('fontSize', 80+(Math.floor(Math.random()*50)));        
+    });
+},fps);
+    
+    
+// jacqui end
+
+
 //phil
   // app.flights = new app.Collection.Flights();
 
